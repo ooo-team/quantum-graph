acb05e9a0539cfa588fdd4c732a3fc2a7be8bd35
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-10-23 14:01:17 +0300

    dataset cleared out of logic mistakes

--- TOOLS/check_data.py
+++ TOOLS/check_data.py
@@ -1,9 +1,11 @@
 import json
+from os import stat
 
 with open('../stations.json', 'r') as st_file:
     data = json.load(st_file)
 
 stations = data['stations']
+
 
 def find_station(name):
     for station in enumerate(stations):
@@ -11,10 +13,39 @@ def find_station(name):
             return station[0]
     pass
 
+
 for station in stations:
     for to_station in station['able_to_get_to']:
         if find_station(to_station['to']) == None:
-            print('somethong bad happaned on', station['name'], 'with', to_station['to'])
+            print('somethong bad happaned on',
+                  station['name'], 'with', to_station['to'])
             continue
-        if stations[find_station(to_station['to'])]['name'] != to_station['to']:
-            print('no backwards connection between', station['name'], to_station['to'])
+
+for station in stations:
+    if 'transfer' in station:
+        for to_station in station['transfer']:
+            if find_station(to_station['to']) == None:
+                print('somethong bad happaned on transfer on',
+                      station['name'], 'with', to_station['to'])
+                continue
+
+
+for station in stations:
+    for to_station in station['able_to_get_to']:
+        for deep_station in stations[find_station(to_station['to'])]['able_to_get_to']:
+            if deep_station['to'] == station['name']:
+                if deep_station['time'] != to_station['time']:
+                    print(
+                        f'not equal travel times on {to_station["to"]} and {deep_station["to"]} ({deep_station["time"]} != {to_station["time"]})')
+
+for station in stations:
+    if 'transfer' in station:
+        for to_station in station['transfer']:
+            if 'transfer' in stations[find_station(to_station['to'])]:
+                for deep_station in stations[find_station(to_station['to'])]['transfer']:
+                    if deep_station['to'] == station['name']:
+                        if deep_station['time'] != to_station['time']:
+                            print(
+                                f'not equal travel times on {to_station["to"]} and {deep_station["to"]} ({deep_station["time"]} != {to_station["time"]})')
+
+print(len(stations))
