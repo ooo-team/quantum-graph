d36a0a1913da2d383fd81678103f6a73cc917cc8
Boyar <boyarnikov@gmail.com>
2021-10-23 20:04:44 +0600

    solve_text_case now return tuple (vertexes, times) see ya boiiiiiiiiiis >_O

--- Scripts/test_task.py
+++ Scripts/test_task.py
@@ -4,14 +4,16 @@ from QUBO import *
 import json
 
 
-def solve_text_case(start):
+def create_solver_connection():
     PARAMS = {
         "remote_addr": "https://remote.qboard.tech",
         "access_key": "8a9c6702-ee8c-4cdc-a857-801f06cbd886"
     }
-
     s = Solver(mode="remote:simcim", params=PARAMS)
+    return s
 
+
+def solve_text_case(start, s):
     with open("../test_stations.json", 'r', encoding='utf-8') as file:
         data = json.load(file)
 
@@ -25,15 +27,16 @@ def solve_text_case(start):
         for item in data['stations'][i]['able_to_get_to']:
             G.add_edge(names[data['stations'][i]['name']], names[item['to']], item['time'])
 
-    for i in range(n):
-        print(G.edges[i])
+    #for i in range(n):
+    #    print(G.edges[i])
 
     np.save("adjacency.npy", Graph.edges)
 
     QUBO_obj = QUBOMatrixFromGraphComm(n, G, start=names[start])
     Q = QUBO_obj.get_matrix()
-
+    file.close()
     np.save("Q.npy", Q)
+    print(len(Q), "len Q")
     # Getting results
     spins, energy = s.solve_qubo(Q, timeout=1)
 
@@ -41,6 +44,8 @@ def solve_text_case(start):
 
     out = list()
     vertexes = []
+    times = []
+
     for i in range(QUBO_obj.get_path_size()):
         stant = QUBO_obj.print_option(spins[i * QUBO_obj.get_row_size():(i + 1) * QUBO_obj.get_row_size()])
         if stant != 'f':
@@ -52,6 +57,7 @@ def solve_text_case(start):
     final_time = 0
 
     for i in range(len(vertexes) - 1):
+        times.append(G.edges[vertexes[i]][vertexes[i + 1]])
         final_time += G.edges[vertexes[i]][vertexes[i + 1]]
     print(final_time, "время на путь")
-    return out
+    return out, times
