b7d4a720815efb871da28ac49c28deb54941fefd
dancing_with_wolves <dimikson4@gmail.com>
2021-10-24 02:53:47 +0300

    before updating python scripts; hope didn't break anything >_<

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open('test_stations.json', 'r', encoding='utf-8') as metro_graph:
     stations = json.load(metro_graph)['stations']
 
 appropriate_stations = [i["name"] for i in stations]
-
+chosen_stations = []
 slver = test_task.create_solver_connection()
 
 
@@ -19,15 +19,18 @@ def maps():
 
 @app.route('/', methods=['POST'])
 def maps_post():
-    text = request.form['station']
-    if text not in appropriate_stations:
-        return render_template("mapbasics.html", data=stations, msg="Некорректный выбор!"), 400
-    processed = test_task.solve_text_case(text, slver)
-    total_time = sum(processed[1])
-    processed[1].append(None)
-    processed_text_time = list(zip(processed[0], processed[1]))
-    # print(list(processed_text_time))
-    return render_template('mapbasics.html', data=stations, processed_text_time=processed_text_time, total_time=total_time), 201
+    if request.form["btn"]=="submit_stations":
+        text = request.form['station']
+        if text not in appropriate_stations:
+            return render_template("mapbasics.html", data=stations, msg="Некорректный выбор!"), 400
+        processed = test_task.solve_text_case(text, slver)
+        total_time = sum(processed[1])
+        processed[1].append(None)
+        processed_text_time = list(zip(processed[0], processed[1]))
+        # print(list(processed_text_time))
+        return render_template('mapbasics.html', data=stations, processed_text_time=processed_text_time, total_time=total_time), 201
+
+        
 
 
 if __name__ == '__main__':
