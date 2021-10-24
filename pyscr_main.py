4fa47500ad09de84a95ac8ddd2c8dc20889374b4
Boyar <boyarnikov@gmail.com>
2021-10-24 06:56:00 +0600

    fixed inexation for shrink task I am going to die

--- pyscr_main.py
+++ pyscr_main.py
@@ -6,7 +6,6 @@ solver = pyscr_solver_logic.create_solver_connection()
 
 print(pyscr_custom_task.solve_case(solver, "Белорусская5", "Белорусская2",
                              points=["Краснопресненская", "Киевская5", "Октябрьская5", "Комсомольская5"]))
-print(pyscr_custom_task.solve_case(solver, "Белорусская5",
-                             points=["Краснопресненская", "Киевская5", "Октябрьская5", "Комсомольская5"]))
+print(pyscr_custom_task.solve_case(solver, "Курская5", "Пушкинская", ["Курская5", "Пушкинская", "Белорусская2", "Павелецкая5"]))
 print(pyscr_test_task.solve_text_case(solver, "Третьяковская", "Охотный ряд"))
-print(pyscr_test_task.solve_text_case(solver, "Охотный ряд"))
+print(pyscr_test_task.solve_text_case(solver, "Кропоткинская"))
