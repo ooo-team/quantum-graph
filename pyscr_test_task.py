442a2a4aed5ca55651932eb7d4aeae36784da2e2
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-10-24 09:39:15 +0300

    csv output added

--- pyscr_test_task.py
+++ pyscr_test_task.py
@@ -3,7 +3,8 @@ from pyscr_QUBO import *
 import json
 
 
-def solve_text_case(s, start, end=-1):  # тесттаска, 5 станций, проехать по всем со стартовой по циклу или до какой-то
+# тесттаска, 5 станций, проехать по всем со стартовой по циклу или до какой-то
+def solve_text_case(s, start, end=-1):
     if end == -1:
         end = start
 
@@ -18,15 +19,24 @@ def solve_text_case(s, start, end=-1):  # тесттаска, 5 станций, 
     G = Graph(n)
     for i in range(n):
         for item in data['stations'][i]['able_to_get_to']:
-            G.add_edge(names[data['stations'][i]['name']], names[item['to']], item['time'])
+            G.add_edge(names[data['stations'][i]['name']],
+                       names[item['to']], item['time'])
 
-    QUBO_obj = QUBOMatrixFromGraphComm(n, G, start=names[start], end=names[end])  # генерим QUBO
+    QUBO_obj = QUBOMatrixFromGraphComm(
+        n, G, start=names[start], end=names[end])  # генерим QUBO
     Q = QUBO_obj.get_matrix()  # достаём его из генератора
 
-    spins, energy = s.solve_qubo(Q, timeout=1)  # пускаем на солвера Q, спины это наш Х, енергия может пригодиться
+    # пускаем на солвера Q, спины это наш Х, енергия может пригодиться
+    spins, energy = s.solve_qubo(Q, timeout=1)
 
-    np.save("test_logs/adjacency.npy", Graph.edges)  # логируем граф (для тесттаски)
+    # логируем граф (для тесттаски)
+    np.save("test_logs/adjacency.npy", Graph.edges)
     np.save("test_logs/Q.npy", Q)  # логируем Q (тож для тесттаски)
     np.save("test_logs/spins.npy", spins)  # Х тоже залогируем
-
-    return QUBO_obj.format_x_to_ret(spins, data, names, start, G)
+    ways = QUBO_obj.format_x_to_ret(spins, data, names, start, G)
+    import csv
+    file = open('test_logs/answer.csv', 'w+', newline='')
+    with file:
+        write = csv.writer(file)
+        write.writerows([ways[0]])
+    return ways
